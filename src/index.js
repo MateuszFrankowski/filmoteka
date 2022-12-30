495eb240d51f426ba0939f138929575b287c0b0d
Wojtek-A <wojtek.a@hotmail.com>
2022-12-30 12:54:55 +0100

    Footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { fetchTheMovieDBList, fetchTheMovieDBMovie } from './js/api';
 import './sass/main.scss';
-import { pagination } from './js/pagination';
 import { footerModalFunction } from './js/footerModal';
 
 footerModalFunction();
