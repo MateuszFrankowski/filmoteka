695c11b778a97139e3453bedc30fdcc03eb345ce
Wojtek-A <wojtek.a@hotmail.com>
2022-12-30 12:11:33 +0100

    Fotter + Modal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import { fetchTheMovieDBList, fetchTheMovieDBMovie } from './js/api';
 import './sass/main.scss';
 import { pagination } from './js/pagination';
+import { footerModalFunction } from './js/footerModal';
+
+footerModalFunction();
