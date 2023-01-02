5b388fb1e2b9c8bc8f40363ff0962bfa6d8bdea0
Wiola Janczura <wiola.janczura@wp.pl>
2023-01-02 09:08:28 +0100

    update

--- src/js/filmDescription.js
+++ src/js/filmDescription.js
@@ -12,13 +12,19 @@ import { fetchTheMovieDBMovie } from './api'
   modal.classList.remove('is-hidden')
  }
 
-const func = async () => {
+const modalMovieInfo = async () => {
    const movie = await fetchTheMovieDBMovie(76600);
+   const moviesArray = movie.data.map(movie => {
+    return movie;
+  });
+  console.log(moviesArray);
+  moviesArray.forEach(() => {
     const markup = `<img class="modal__poster" src="${movie.poster_path}"
     alt="poster image of ${movie.title}" />
     <h2 class="modal__title">${title}</h2> `;
     modal.innerHTML = markup;
-  }; 
+  })}; 
 
+  modalMovieInfo()
 
 
