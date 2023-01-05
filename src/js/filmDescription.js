4b17a87dc6486b63a77f7dfcaa751c3dff303c72
MateuszFrankowski <mateuszfrankowski91@gmail.com>
2023-01-05 19:42:33 +0100

    film description - handling when no login user

--- src/js/filmDescription.js
+++ src/js/filmDescription.js
@@ -14,7 +14,8 @@ const watchedHandler = event => {
   if (event.target.innerText === 'ADD TO WATCHED') {
     updateUserWatchedData(window.userUid, movie.id, true);
     event.target.innerText = 'REMOVE FROM WATCHED';
-    addToQueueBtn.innerText = 'ADD TO QUEUE';
+    addToWatchBtn.innerText = addToQueueBtn.innerText = 'ADD TO QUEUE';
+    ('REMOVE FROM QUEUE');
   } else {
     updateUserWatchedData(window.userUid, movie.id, false);
     event.target.innerText = 'ADD TO WATCHED';
@@ -67,13 +68,15 @@ function showModal() {
 export const modalMovieInfo = async movieId => {
   showModal();
   movie = await fetchTheMovieDBMovie(movieId);
-  const firebaseFilms = await fetchUserDataFromFirestore(window.userUid);
-  const { filmWatched, filmInQueue } = checkIfFilmInBase(
-    firebaseFilms,
-    movieId
-  );
 
-  const markup = `
+  const firebaseFilms = await fetchUserDataFromFirestore(window.userUid);
+  if (window.userSigned) {
+    const { filmWatched, filmInQueue } = checkIfFilmInBase(
+      firebaseFilms,
+      movieId
+    );
+
+    const markup = `
 <div class="modal">
 <button class="modal__close-btn" data-modal-close>
 <svg viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m8 8 14 14M8 22 22 8" stroke="#000" stroke-width="2"/></svg>
@@ -111,16 +114,60 @@ export const modalMovieInfo = async movieId => {
 </div>
 </div>`;
 
-  modal.innerHTML = markup;
-  addToWatchBtn = document.querySelector('.modal__watched-btn');
-  addToQueueBtn = document.querySelector('.modal__queue-btn');
-  addToWatchBtn.addEventListener('click', event => {
-    watchedHandler(event);
-  });
-  addToQueueBtn.addEventListener('click', event => {
-    queueHandler(event);
-  });
-  const closeModalBtn = document.querySelector('[data-modal-close]');
-  closeModalBtn.addEventListener('click', showModal);
+    modal.innerHTML = markup;
+    addToWatchBtn = document.querySelector('.modal__watched-btn');
+    addToQueueBtn = document.querySelector('.modal__queue-btn');
+    addToWatchBtn.addEventListener('click', event => {
+      watchedHandler(event);
+    });
+    addToQueueBtn.addEventListener('click', event => {
+      queueHandler(event);
+    });
+    const closeModalBtn = document.querySelector('[data-modal-close]');
+    closeModalBtn.addEventListener('click', showModal);
+  } else {
+    const markup = `
+<div class="modal">
+<button class="modal__close-btn" data-modal-close>
+<svg viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m8 8 14 14M8 22 22 8" stroke="#000" stroke-width="2"/></svg>
+</button>
+<img class="modal__poster" src="${movie.poster_path}"
+    alt="poster image of ${movie.title}" />
+    <div class="modal__description">
+    <h2 class="modal__title">${movie.title}</h2> 
+    <ul class="modal__list">
+    <li class="modal__items">
+        <p class="modal__info">Vote / Votes</p>
+        <p class="modal__data"><span class="modal__vote">${movie.vote_average}</span><span class="modal__info">/</span>${movie.vote_count}</p>
+    </li>
+    <li class="modal__items">
+        <p class="modal__info">Popularity</p>  
+        <p class="modal__data">${movie.popularity}</p>
+    </li>
+    <li class="modal__items">
+        <p class="modal__info">Original Title</p>
+        <p class="modal__data">${movie.original_title}</p>
+   </li>
+    <li class="modal__items">
+        <p class="modal__info">Genre</p>
+        <p class="modal__data">${movie.genres}</p>
+    </li>
+</ul>
+<div>
+    <p class="modal__about">ABOUT</p>
+    <p class="modal__more">${movie.overview}</p>
+    </div>
+<div class="modal__add-btns">
+<button class="modal__watched-btn">'ADD TO WATCHED'</button>
+<button class="modal__queue-btn">'ADD TO QUEUE'</button>
+</div>
+</div>
+</div>`;
+
+    modal.innerHTML = markup;
+
+    const closeModalBtn = document.querySelector('[data-modal-close]');
+    closeModalBtn.addEventListener('click', showModal);
+  }
   //deleteUserData(window.userUid);
 };
