1f7ac430653ee8015c7da1b5188859c908d25af3
Michał Murawski <m.a.murawski@wp.pl>
2023-01-12 23:50:00 +0100

    change no results data and remove console.logs

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,38 +1,12 @@
-import { fetchTheMovieDBList } from './apiFetch';
-import { addLoaderSpinner } from './loaderSpinner';
 import { modalMovieInfo } from './filmDescription';
 
-// LOADER SPINNER TO ADD
-// loaderSpinner.classList.add('loader');
-
-// LOADER SPINNER TO REMOVE
-// loaderSpinner.classList.remove('loader');
-
-// dataMovies = {
-//        page: 1, // nr of actual page
-//        totalPages: 20, // nr of total pages
-//        query: "", // searching text in home
-//      };
-
-//  await fetchTheMovieDBList(1, "avatar");
-
-// const movies = async () => {
-//     const movies = await fetchTheMovieDBList(2);
-//     console.log(movies) //list of movies
-//     ... <!-- use result -->
-// }
-//gallery
 export const createMovies = async movies => {
   const galleryContainer = document.getElementsByClassName('gallery');
-  // console.log(galleryContainer[0]);
-  // console.log(movies);
   let i = 0;
-  const markup = movies.data.map(
-    data => {
-      i += 1;
-      return liElement(data, i)
-    }
-  );
+  const markup = movies.data.map(data => {
+    i += 1;
+    return liElement(data, i);
+  });
 
   galleryContainer[0].classList.add('grid');
   galleryContainer[0].innerHTML = markup.join('');
@@ -40,61 +14,24 @@ export const createMovies = async movies => {
 
 export const liElement = (data, movieNr) => {
   return `<li  data-film_id="${data.id}" data-film_nr="${movieNr}">
-              <figure class="card">
-                  <div class="thumb" data-id="${data.id}">
-
-                  <img class="img" src="${data.poster_path
-        }" />
-
-                  </div>
-                  <figcaption>
-                  <h3 class="title">${data.title}</h3>
-                  <div class="details-wrapper">
+            <figure class="card">
+              <div class="thumb" data-id="${data.id}">
+                <img class="img" src="${data.poster_path}" />
+              </div>
+              <figcaption>
+                <h3 class="title">${data.title}</h3>
+                <div class="details-wrapper">
                   <p>${data.genres.join(', ')}</p>|
                   <p>${data.release_year}</p>
                   <div class="rating rating--visible">${roundTo1Comma(
-          data.vote_average
-        )}</div>
-                  </div>
-                  </figcaption>
-              </figure>
-        </li>
-          `
-}
-
-//-------------- Function rounding rating to 1 place after comma--------------//
-// export function roundingMethodToFirstPlace(value) {
-//     let roundingValue = Number(Math.round(value + 'e+1') + 'e-1');
-//   return roundingValue
-//   };
-//-----------------------------------------------
-// const moviesArray = movies.data.map(movie => {
-//   return movie.title;
-// });
-// console.log(fetchTheMovieDB);
-//   const markup = `
-// <li>
-//   <figure class="card">
-//     <div class="thumb" data-id="${data.id}">
-//       <img class="img" src="https://www.themoviedb.org/t/p/w500${
-//         data.poster_path
-//       }" />
-//     </div>
-//     <figcaption>
-//       <h3 class="title">${data.title}</h3>
-//       <div class="details-wrapper">
-//         <p class="details" data-film_id="${data.id}">${data.genres.join(
-//     ', '
-//   )} &#124; ${data.release_year}</p>
-//         <div class="rating rating--visible">${roundTo1Comma(
-//           data.vote_average
-//         )}</div>
-//       </div>
-//     </figcaption>
-//   </figure>
-// </li>
-// `;
-//   libraryGrid.insertAdjacentHTML('beforeend', markup);
+                    data.vote_average
+                  )}</div>
+                </div>
+              </figcaption>
+            </figure>
+          </li>
+        `;
+};
 
 export const roundTo1Comma = num => {
   let roundNum;
@@ -116,9 +53,7 @@ export const clickGallery = () => {
     }
     const movieId = goToLiElement(e.target).dataset.film_id;
     const movieNr = goToLiElement(e.target).dataset.film_nr;
-    // console.log(movieId, movieNr);
     modalMovieInfo(movieId, movieNr);
-    // function to open modal with movie id
   });
 
   const goToLiElement = targetELement => {
